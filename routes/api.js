cbfde89b5e5d30f40dd7edaa591e72fd0b8cb854
Emon Reza <rifatbinreza@gmail.com>
2019-12-07 18:43:10 +0600

    api/user/add route add

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/order");
+const userController = require("../controllers/user");
 
 router.post("/order/add", orderController.addOrder);
+router.post("/user/add", userController.addUser);
 
 module.exports = router
